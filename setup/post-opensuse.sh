8e73fbdaeaaea5791d8b4a4aa1a92bb7905f37f0
Luke Perry <lukeperry144@gmail.com>
2026-09-16 17:11:46 +0000

    Tune ghostty/fastfetch config, drop kitty, add oh-my-zsh to installers
    - Replace fastfetch's default ML4W logo/layout with a custom fastfetch
      config and ASCII logo (giuseppe.txt).
    - Tune ghostty appearance (padding balance, colorspace, maximize on
      launch, etc.) now that it's the default terminal.
    - Remove kitty.conf now that ghostty has fully replaced kitty.
    - Add an "Oh My Zsh" install step to post-arch.sh, post-fedora.sh and
      post-opensuse.sh, mirroring the existing Oh My Posh step and the
      logic already used by ml4w-change-shell, so a fresh install doesn't
      hit a missing $ZSH/oh-my-zsh.sh error on first zsh launch.

--- setup/post-opensuse.sh
+++ setup/post-opensuse.sh
@@ -36,6 +36,33 @@ sudo zypper --non-interactive --gpg-auto-import-keys install quickshell
 curl -s https://ohmyposh.dev/install.sh | bash -s -- -d ~/.local/bin
 
 # --------------------------------------------------------------
+# Oh My Zsh
+# --------------------------------------------------------------
+
+if [ ! -d "$HOME/.oh-my-zsh" ]; then
+    echo ":: Installing oh-my-zsh"
+    sh -c "$(curl -fsSL https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/install.sh)" "" --unattended
+    cp $repo_path/dotfiles/.zshrc ~/.zshrc
+else
+    echo ":: oh-my-zsh already installed"
+fi
+
+if [ ! -d "$HOME/.oh-my-zsh/custom/plugins/zsh-autosuggestions" ]; then
+    echo ":: Installing zsh-autosuggestions"
+    git clone https://github.com/zsh-users/zsh-autosuggestions ${ZSH_CUSTOM:-$HOME/.oh-my-zsh/custom}/plugins/zsh-autosuggestions
+fi
+
+if [ ! -d "$HOME/.oh-my-zsh/custom/plugins/zsh-syntax-highlighting" ]; then
+    echo ":: Installing zsh-syntax-highlighting"
+    git clone https://github.com/zsh-users/zsh-syntax-highlighting.git ${ZSH_CUSTOM:-$HOME/.oh-my-zsh/custom}/plugins/zsh-syntax-highlighting
+fi
+
+if [ ! -d "$HOME/.oh-my-zsh/custom/plugins/fast-syntax-highlighting" ]; then
+    echo ":: Installing fast-syntax-highlighting"
+    git clone https://github.com/zdharma-continuum/fast-syntax-highlighting.git ${ZSH_CUSTOM:-$HOME/.oh-my-zsh/custom}/plugins/fast-syntax-highlighting
+fi
+
+# --------------------------------------------------------------
 # ML4W Settings App
 # --------------------------------------------------------------
 
